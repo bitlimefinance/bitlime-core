a7922a6094ec7e85833f789d192b6517f6f95925
marcosolito <bitlime.finance@gmail.com>
2023-01-09 11:13:48 +0100

    Force Lime in the pool and allow to hide this features by enabling allowAlienPools

--- contracts/BitlimeV2Factory.sol
+++ contracts/BitlimeV2Factory.sol
@@ -9,14 +9,17 @@ contract BitlimeV2Factory is IBitlimeV2Factory {
     uint public fee = 25;
     address public feeTo;
     address public feeToSetter;
+    address public Lime;
+    bool public allowAlienPools = false;
 
     mapping(address => mapping(address => address)) public getPair;
     address[] public allPairs;
 
     event PairCreated(address indexed token0, address indexed token1, address pair, uint);
 
-    constructor(address _feeToSetter) public {
+    constructor(address _feeToSetter, address _Lime) public {
         feeToSetter = _feeToSetter;
+        Lime = _Lime;
     }
 
     function allPairsLength() external view returns (uint) {
@@ -25,6 +28,7 @@ contract BitlimeV2Factory is IBitlimeV2Factory {
 
     function createPair(address tokenA, address tokenB) external returns (address pair) {
         require(tokenA != tokenB, 'BitlimeV2: IDENTICAL_ADDRESSES');
+        require(tokenA == Lime || tokenB == Lime || allowAlienPools, 'BitlimeV2: LIME_IS_NECESSARY');
         (address token0, address token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
         require(token0 != address(0), 'BitlimeV2: ZERO_ADDRESS');
         require(getPair[token0][token1] == address(0), 'BitlimeV2: PAIR_EXISTS'); // single check is sufficient
@@ -54,5 +58,10 @@ contract BitlimeV2Factory is IBitlimeV2Factory {
     function setFeeToSetter(address _feeToSetter) external {
         require(msg.sender == feeToSetter, 'BitlimeV2: FORBIDDEN');
         feeToSetter = _feeToSetter;
+    }
+
+    function setAlienPools(bool _allowAlienPools) external {
+        require(msg.sender == feeToSetter, 'BitlimeV2: FORBIDDEN');
+        allowAlienPools = _allowAlienPools;
     }
 }
